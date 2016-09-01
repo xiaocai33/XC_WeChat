1a5b890ecbcad9dbe94bb57ef1632f637802da74
xiaocai <tycai1@sina.com>
2016-09-01 17:14:08 +0800

    初始化项目

---
+++ XC_WeChat/XC_WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XC_WeChat
+//
+//  Created by 小蔡 on 16/9/1.
+//  Copyright © 2016年 xiaocai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
